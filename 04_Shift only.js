9b8a2415b392b01310aa9b30d8b3ca055f1f9854
畔上　高典 <takanori.azegami.ta@ultmarc.jp>
2022-12-09 13:42:27 +0900

    no message

--- 04_Shift only.js
+++ 04_Shift only.js
@@ -1,7 +1,7 @@
 // inputに入力データ全体が入る
 function Main(input) {
 	// 1行目がinput[0], 2行目がinput[1], …に入る
-	let input = input.split("\n");
+	input = input.split("\n");
 	let tmp = input[1].split(" ");
 
 	//文字列から10進数に変換するときはparseIntを使います
