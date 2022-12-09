841adc30b523a10a505de4c4c736bd19923f8b62
takanori azegami <81016034+takanori-azegami-jp@users.noreply.github.com>
2022-12-09 13:39:12 +0900

    Update 04_Shift only.js

--- 04_Shift only.js
+++ 04_Shift only.js
@@ -1,8 +1,8 @@
 // inputに入力データ全体が入る
 function Main(input) {
 	// 1行目がinput[0], 2行目がinput[1], …に入る
-	input = input.split("\n");
-	tmp = input[1].split(" ");
+	let input = input.split("\n");
+	let tmp = input[1].split(" ");
 
 	//文字列から10進数に変換するときはparseIntを使います
 	let a = parseInt(input[0], 10);
