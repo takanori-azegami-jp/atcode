783a23f67705bd62c3a0017a5d6a0a2b5952ffb4
畔上　高典 <takanori.azegami.ta@ultmarc.jp>
2023-03-30 17:14:33 +0900

    no message

--- AtCoderBeginnersSelection/04_Shift only.js
+++ AtCoderBeginnersSelection/04_Shift only.js
@@ -7,34 +7,15 @@
 // inputに入力データ全体が入る
 function Main(input) {
 	// 1行目がinput[0], 2行目がinput[1], …に入る
-	input = input.split("\n");
-	let tmp = input[1].split(" ");
+	let nums = input.split('\n')[1].split(' ').map((n) => parseInt(n, 10));
 
-	//文字列から10進数に変換するときはparseIntを使います
-	let a = parseInt(input[0], 10);
-	let b=[], c=0, d=false, n=0;
-	for (let i = 0; i < 200; i++){
-		if ( i === 0 ){
-			c = 1;
-		}else{
-			c = 2 ** i;
-		}
-		for (let j=0; j < a; j++){
-			b[j] = parseInt(tmp[j],10) / c;
-			// 奇数/偶数判定
-			if( b[j] % 2 !== 0 ){
-				//奇数
-				d = true;
-			}
-		}
-		if( d === false){
-			n++;
-		}else{
-			break;
-		}
+	let count = 0;
+	while(nums.every((n) => (n % 2) === 0)) {
+		count++;
+		nums = nums.map((n) => n / 2);
 	}
-	//出力
-	console.log(n);
+	console.log(count);
+
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
